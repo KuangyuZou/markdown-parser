c93265d4d3c659054aa55f632879a32f6fa3e240
KuangyuZou <97483691+KuangyuZou@users.noreply.github.com>
2022-04-21 08:53:57 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,7 +19,7 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-//print line;line++
+
         }
 
         return toReturn;
