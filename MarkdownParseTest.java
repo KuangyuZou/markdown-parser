49230d46c532aa44e2b22d4da08a192b18624a6c
KuangyuZou <97483691+KuangyuZou@users.noreply.github.com>
2022-04-21 11:16:47 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,16 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
 public class  MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinks(){
+        List result = List.of("https://something.com", "some-page.html");
+        assertEquals(result, MarkdownParse.getLinks("test-file.md"));
     }
     
 }
